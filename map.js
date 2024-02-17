3f41d511a69b95e418caf990a10a4835c539d926
Sirinat <159916599+SrnSuko@users.noreply.github.com>
2024-02-17 18:01:17 +0700

    Add files via upload

--- map.js
+++ map.js
@@ -13,6 +13,7 @@ var span = document.getElementsByClassName("close")[0];
 span.onclick = function() { 
   modal3.style.display = "none";
 }
+
 var modal8 = document.getElementById("Modal8");
 
 var img8 = document.getElementById("Icon8");
@@ -26,4 +27,19 @@ var span = document.getElementsByClassName("close")[1];
 
 span.onclick = function() { 
   modal8.style.display = "none";
+}
+
+var modaltemp = document.getElementById("ModalTemp");
+
+var imgtemp = document.getElementById("IconTemp");
+var modalImgtemp = document.getElementById("IconTemp");
+imgtemp.onclick = function(){
+  modaltemp.style.display = "block";
+  modalImgtemp.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+var span = document.getElementsByClassName("close")[2];
+
+span.onclick = function() { 
+  modaltemp.style.display = "none";
 }
